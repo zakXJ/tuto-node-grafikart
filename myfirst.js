cbc2da2080d67ec35ad06dfc21497c4f77d72add
zakXJ <xj3.zak@gmail.com>
2022-07-11 08:27:42 +0100

    video 4/6

---
+++ myfirst.js
@@ -0,0 +1,39 @@
+var http = require('http')
+let url = require('url')
+
+var server = http.createServer()
+
+/* http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World! TEST');
+}).listen(8080); */
+
+server.on('request', (req, res)=>{
+
+
+  if(url.parse(req.url,true).pathname=='/'){
+    let query = url.parse(req.url,true).query
+    res.writeHead(200)
+    console.log(url.parse(req.url,true)) 
+    res.end('BONJOUR '+query.name)
+    console.log('1')
+  }
+
+
+/* let queryname
+    res.writeHead(200)
+  let query = url.parse(req.url,true).query
+  if(query.name!=undefined){
+    queryname=query.name
+    console.log('query.name!=undefined')
+  }
+  console.log(queryname)
+  console.log(url.parse(req.url,true).query.name)  
+  console.log('1')
+    res.end('BONJOUR')
+    console.log('2') */
+
+}
+)
+
+server.listen(8080)
