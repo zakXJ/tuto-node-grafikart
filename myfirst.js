698e26ea979057b01a2ce35aab73981853b005ab
zakXJ <xj3.zak@gmail.com>
2022-07-11 17:02:04 +0100

    video 5/6

--- myfirst.js
+++ myfirst.js
@@ -1,12 +1,12 @@
-var http = require('http')
+/* var http = require('http')
 let url = require('url')
 
 var server = http.createServer()
 
-/* http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World! TEST');
-}).listen(8080); */
+}).listen(8080); 
 
 server.on('request', (req, res)=>{
 
@@ -20,7 +20,7 @@ server.on('request', (req, res)=>{
   }
 
 
-/* let queryname
+let queryname
     res.writeHead(200)
   let query = url.parse(req.url,true).query
   if(query.name!=undefined){
@@ -31,9 +31,23 @@ server.on('request', (req, res)=>{
   console.log(url.parse(req.url,true).query.name)  
   console.log('1')
     res.end('BONJOUR')
-    console.log('2') */
+    console.log('2') 
 
 }
 )
 
-server.listen(8080)
+server.listen(8080) */
+
+let express = require('express')
+let app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/demo', (req, res) => {
+  res.send('Hello World! DEMO')
+})
+
+app.listen(8080)
+
